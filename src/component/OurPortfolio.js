af7c22e8d39dd897296f34541b696cafa4c66c16
unknown <ninja_2731@hotmail.com>
2021-10-21 12:03:06 +0700

    test

--- src/component/OurPortfolio.js
+++ src/component/OurPortfolio.js
@@ -10,32 +10,32 @@ const OurPortfolio = () => {
 
         <div className="imageGroup">
           <div className="image">
-            <img src="/myimg/Image-1.png" />
+            <img src="/myimg/Image-1.png" alt="img1" />
             <h4>Ebony & Ivory</h4>
             <p>Branding</p>
           </div>
           <div className="image">
-            <img src="/myimg/Image-2.png" />
+            <img src="/myimg/Image-2.png" alt="img2" />
             <h4>Smart Stationary</h4>
             <p>Print Design</p>
           </div>
           <div className="image">
-            <img src="/myimg/Image-3.png" />
+            <img src="/myimg/Image-3.png" alt="img3" />
             <h4>Smart Stationary</h4>
             <p>Print Design</p>
           </div>
           <div className="image">
-            <img src="/myimg/Image-4.png" />
+            <img src="/myimg/Image-4.png" alt="img4" />
             <h4>Vinyl Recordy</h4>
             <p>Product Mock-Up</p>
           </div>
           <div className="image">
-            <img src="/myimg/Image-5.png" />
+            <img src="/myimg/Image-5.png" alt="img5" />
             <h4>Treehouse Template</h4>
             <p>Web Design</p>
           </div>
           <div className="image">
-            <img src="/myimg/Image-6.png" />
+            <img src="/myimg/Image-6.png" alt="img6" />
             <h4>Treehouse Template</h4>
             <p>Web Design</p>
           </div>
