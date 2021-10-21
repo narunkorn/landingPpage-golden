78b51f42207dbedeb41d9d3a6aa5a4ce4cb120d9
unknown <ninja_2731@hotmail.com>
2021-10-21 12:59:08 +0700

    testimg

---
+++ src/component/About.js
@@ -0,0 +1,11 @@
+import "./About.css";
+const About = () => {
+  return (
+    <div className="containerAbout">
+      <div className="contentAbout">
+        <img src="myImg/forest.jpg" />
+      </div>
+    </div>
+  );
+};
+export default About;
--- src/page/Page.js
+++ src/page/Page.js
@@ -1,12 +1,14 @@
 import Header from "../component/Header";
 import Service from "../component/Service";
 import OurPortfolio from "../component/OurPortfolio";
+import About from "../component/About";
 const Page = () => {
   return (
     <div>
       <Header />
       <Service />
       <OurPortfolio />
+      <About />
     </div>
   );
 };
